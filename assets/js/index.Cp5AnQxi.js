1beacbd5283dd7164730ed1fc0c154b2f4a02f62
hoangndm95 <hoangndm95@gmail.com>
2025-06-09 14:54:52 +0700

    Updates

---
+++ assets/js/index.Cp5AnQxi.js
@@ -0,0 +1,2 @@
+import{j as o,L as s}from"./index.3GC5Wfs2.js";const e=()=>o.jsxs("div",{children:[o.jsx("h1",{children:"404 - Page Not Found"}),o.jsx(s,{to:"/",children:"Go back to home"})]});export{e as default};
+//# sourceMappingURL=index.Cp5AnQxi.js.map
