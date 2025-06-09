c6d0bdefd09329f05346f19351e6ab5f3811ad7e
hoangndm95 <hoangndm95@gmail.com>
2025-06-09 14:42:04 +0700

    Updates

---
+++ assets/js/index.DVgiRFlc.js
@@ -0,0 +1,2 @@
+import{j as o,L as s}from"./index.CuXRzN-f.js";const e=()=>o.jsxs("div",{children:[o.jsx("h1",{children:"404 - Page Not Found"}),o.jsx(s,{to:"/",children:"Go back to home"})]});export{e as default};
+//# sourceMappingURL=index.DVgiRFlc.js.map
