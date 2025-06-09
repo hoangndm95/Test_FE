e2765ac41cebe017dcf1909b2dcf7b7c1652826b
hoangndm95 <hoangndm95@gmail.com>
2025-06-09 14:53:03 +0700

    Updates

--- assets/js/index.D8Wq6oP_.js
+++
@@ -1,2 +0,0 @@
-import{j as o,L as s}from"./index.C3YDPvl7.js";const e=()=>o.jsxs("div",{children:[o.jsx("h1",{children:"404 - Page Not Found"}),o.jsx(s,{to:"/",children:"Go back to home"})]});export{e as default};
-//# sourceMappingURL=index.D8Wq6oP_.js.map
